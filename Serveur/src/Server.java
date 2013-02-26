d0fbdd24427bec0b21bcdc4eb4d6c6e96271e5fe
Erset <jerome.p24@free.fr>
2013-02-26 18:59:24 +0100

    Debut de mise en place de la connexion server-client

--- Serveur/src/Server.java
+++ Serveur/src/Server.java
@@ -1,6 +1,11 @@
 import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 
 public class Server extends Thread{
@@ -8,9 +13,18 @@ public class Server extends Thread{
 	private ArrayList<Game> games;
 	private ArrayList<Player> players;
 
+	private ServerSocket serverSocket;
+	private Socket socket;
+
 	public Server(){
 		games = new ArrayList<Game>();
 		players = new ArrayList<Player>();
+		try {
+			serverSocket = new ServerSocket(8448);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		players.add(new Player("Test", "0", false));
 		games.add(new Game(players.get(0), null));
@@ -18,6 +32,13 @@ public class Server extends Thread{
 
 	public void run() {
 		games.get(0).start();
+		//connection.start();
+		try {
+			System.out.println("L'adresse locale est : "+InetAddress.getLocalHost() );
+		} catch (UnknownHostException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} 
 
 		Timer timer = new Timer();
 		timer.schedule(new TimerTask() {
@@ -28,7 +49,16 @@ public class Server extends Thread{
 		}, 0, 10000);
 
 		while(true){
+			try {
+				socket = serverSocket.accept();
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+			System.out.println("Un client essaye de se connecter");
 
+			Connection connec = new Connection(this, socket);
+			connec.start();
 
 			try {
 				sleep(16);
@@ -42,5 +72,10 @@ public class Server extends Thread{
 	public void createGame(Player creator){
 		if(creator.getGame() == null)
 			games.add(new Game(players.get(0), null));
+	}
+
+	public void createPlayer(String login, Socket socket)
+	{
+		//players.add(new Player())
 	}
 }
