d147bf1bdf0c03f8d34a4232bdeaa99995be659a
matbx33 <matmatdu24@gmail.com>
2013-02-26 21:33:12 +0100

    Ajout de commander.java

---
+++ Serveur/src/Commander.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+
+public class Commander extends Thread {
+	ArrayList<String> myHistory;
+	private boolean myStopSignal;
+	
+	public Commander() {
+		myHistory = new ArrayList<String>();
+		myStopSignal = false;
+	}   
+	
+	@Override
+	public void run() {
+		while(!myStopSignal) {
+			System.out.println("Test");
+		}
+	}
+	
+	public void askCommand(String cmd) {
+		if(cmd.equals("shutdown")) {
+			System.out.println("> Voulez vous vraiment éteindre le serveur ? (encore X parties en cours et X utilisateurs connectés) [O/N]");
+			if(1==1)
+				shutDown();				
+		}
+		else if(cmd.equals("help")) {
+			System.out.println("> Liste des commandes :");
+		}
+		else {
+			System.out.println("> Commande invalide. Tapez \"help\" pour la liste des commandes.");
+		}
+	}
+	
+	public void shutDown() {
+		myStopSignal = true;
+		System.out.println("Extinction du serveur en cours...");
+	}
+	
+}
