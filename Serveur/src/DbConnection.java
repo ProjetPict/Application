a63cf71000cb4495da92ff99514c365fe1e957d2
matbx33 <matmatdu24@gmail.com>
2013-02-28 14:13:00 +0100

    Ajouts des classes nécessaires a la base de données serveur

--- Serveur/src/DbConnection.java
+++ Serveur/src/DbConnection.java
@@ -1,16 +1,23 @@
+import java.sql.SQLException;
 import java.util.ArrayList;
+
+import dbwConnection.DbwConnection;
 
 
 public class DbConnection extends Thread {
 
+	private String db_link_url = "http://localhost/DrawVs/core/dbw.php";
 	private String db_hostname = "127.0.0.1";
 	private String db_username = "root";
 	private String db_password = "";
-	private String db_basename = "drawvs.free.fr";
-	private String db_portnumb = "20";
+	private String db_basename = "drawvs";
 	
 	public DbConnection() {
-		
+		try {
+			DbwConnection conn = new DbwConnection(db_link_url, db_username, db_password, db_hostname, db_basename);
+		} catch(SQLException e) {
+			System.out.println("> Impossible de se connecter à la base de données ("+e+")");
+		}
 	}
 	
 	public boolean tryConnection(String username, String password) {
---
+++ Serveur/src/ServerDatabase.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+
+
+public class ServerDatabase {
+	
+	private ArrayList<Player> servDbPlayerLogs = new ArrayList<Player>();
+	private ArrayList<Statistiques> servDbStatsLogs = new ArrayList<Statistiques>();
+	
+	public ServerDatabase() {
+		
+	}
+	
+	public void loadDatabase() {
+		
+	}
+	
+	public void saveDatabase() {
+		
+	}
+	
+	public void saveStatistiques() {
+		
+	}
+	
+}
