addfb4aee82118f3a70b00ba2154a022b336a144
qpoussie <quentin.poussier@gmail.com>
2013-03-29 16:26:40 +0100

    test de montée en charge du serveur

---
+++ Serveur/src/test/MainPlayerNumberTest.java
@@ -0,0 +1,37 @@
+package test;
+
+/**
+ * Test de montée en charge du serveur. En cas de surcharge, le serveur print une exception.
+ * monte à 2400 sur ma machine
+ * @author Quentin
+ * 
+ */
+public class MainPlayerNumberTest {
+
+	//nombre de connections voulues
+	static int CONNECTTEST = 2000;
+
+	public static void main(String[] args)  {
+		Boolean res = true;
+		Integer i;
+		PlayerNumberTest[] pNT = new PlayerNumberTest[CONNECTTEST];
+		
+		for(i=0; i < CONNECTTEST ; i++){
+			pNT[i] = new PlayerNumberTest();
+			res = pNT[i].connect(i.toString(), "");
+			
+			if(!res)
+				break;
+		}
+
+		System.out.print(i.toString() + " connections réussies, res : " + res);
+		
+		try {
+			Thread.currentThread().sleep(10000)	;
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ Serveur/src/test/PlayerNumberTest.java
@@ -0,0 +1,68 @@
+package test;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import server.Player;
+import socketData.Command;
+
+public class PlayerNumberTest {
+	private Socket drawSocket;
+	private ObjectOutputStream out;
+	private ObjectInputStream in;
+	private static String host = "localhost" ;
+	
+	/**
+	 * @param args
+	 */
+
+	
+	
+	public boolean connect(String login, String password)
+	{
+		
+		
+		Command log = new Command(login);
+		Command pass = new Command(password);
+		
+		try{
+			drawSocket = new Socket(host ,8448);
+			out = new ObjectOutputStream(drawSocket.getOutputStream());
+			in = new ObjectInputStream(drawSocket.getInputStream());
+		}catch (IOException e) {
+			return false;
+		}
+		
+		try{
+			//attente du signal d'envoi et envoi du login
+			
+			out.writeObject(log);
+			out.flush();
+			out.writeObject(pass);
+			out.flush();
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		//attente de la confirmation de connexion
+		try{
+			Object conf = in.readObject();
+			if(!(conf instanceof Command))
+				throw new ClassNotFoundException(); 
+	
+			if(((Command)conf).command.equals("success")){
+				return true;
+			}
+		}catch(ClassNotFoundException e){
+			e.printStackTrace();
+			return false;
+		}catch(IOException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return false;
+	}
+
+}
